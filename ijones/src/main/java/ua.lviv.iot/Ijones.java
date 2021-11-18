e9a32fa0e0a67a52ebaa02a220e46b74620133ca
Meksich <maks10031003@gmail.com>
2021-11-18 13:47:26 +0200

    Add ability once jump in current row

--- ijones/src/main/java/ua.lviv.iot/Ijones.java
+++ ijones/src/main/java/ua.lviv.iot/Ijones.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class Ijones {
     public static Map<String, Integer> cache = new HashMap<>();
     private static Map<Integer, String> floor_ij = new HashMap<>();
+    private static int width_ij;
+    private static List<Integer> colsCounter = new ArrayList<>();
 
     public static int corridor(int curCol, int curRow, char curValue){
         if (curCol == 0){
@@ -16,14 +19,21 @@ public class Ijones {
 
         List<Integer> cols = new ArrayList<>();
         List<Integer> rows = new ArrayList<>();
-        for (int i = 0; i < curCol; i++){
+        for (int i = 0; i <= curCol; i++){
             String word = floor_ij.get(i);
             while (word.indexOf(curValue) > -1){
-                if (i!=curCol-1 && word.indexOf(curValue)!=curRow) {
-                    cols.add(i);
-                    rows.add(word.indexOf(curValue));
-                }
+                int indexOfCurValue = word.indexOf(curValue);
                 word = word.replaceFirst(String.valueOf(curValue), " ");
+                if (i!=curCol-1 && indexOfCurValue!=curRow) {
+                    if (i == curCol && (indexOfCurValue == 0 || indexOfCurValue == width_ij-1 || colsCounter.get(i)==1)) {
+                        continue;
+                    }
+                    cols.add(i);
+                    rows.add(indexOfCurValue);
+                    if (i==curCol)
+                        colsCounter.add(i, 1);
+                }
+
             }
         }
         int result = 0;
@@ -44,7 +54,10 @@ public class Ijones {
 
     public static int goAhead(Map<Integer, String> floor, int width){
         floor_ij = floor;
-        return Ijones.corridorCache(floor.size(), 0, floor.get(floor.size()-1).charAt(0)) +
-                Ijones.corridorCache(floor.size(), width-1, floor.get(floor.size()-1).charAt(width-1));
+        width_ij = width;
+        int bound = floor.size();
+        IntStream.range(0, bound).forEach(i -> colsCounter.add(0));
+        return Ijones.corridorCache(floor.size()-1, 0, floor.get(floor.size()-1).charAt(0)) +
+                Ijones.corridorCache(floor.size()-1, width-1, floor.get(floor.size()-1).charAt(width-1));
     }
 }
